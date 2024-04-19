0eabbd031dd9b4bc284118a8480f3c9e38db3043
Aristeas <94058548+Jnick-24@users.noreply.github.com>
2024-04-19 18:01:32 -0500

    Accidentally gitignored some important files

---
+++ Data/Scripts/AssemblyScripts/DebugUtils/ModularLog.cs
@@ -0,0 +1,68 @@
+﻿using Sandbox.ModAPI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modular_Assemblies.Data.Scripts.AssemblyScripts.Definitions;
+
+namespace Modular_Assemblies.Data.Scripts.AssemblyScripts.Debug
+{
+    /// <summary>
+    /// Standard logging class. Outputs to %AppData%\Roaming\Space Engineers\Storage\
+    /// </summary>
+    public class ModularLog
+    {
+        readonly TextWriter _writer;
+        private static ModularLog I;
+
+        public static void Log(string message)
+        {
+            I._Log(message);
+        }
+        public static void LogException(Exception ex, Type callingType, string prefix = "")
+        {
+            I._LogException(ex, callingType, prefix);
+        }
+
+        public static void Init()
+        {
+            Close();
+            I = new ModularLog();
+        }
+
+        private ModularLog()
+        {
+            _writer = MyAPIGateway.Utilities.WriteFileInGlobalStorage("ModularAssemblies.log"); // Only creating one debug.log to avoid clutter. Might change in the future.
+            _writer.WriteLine($"Modular Assemblies v{ApiHandler.ModVersion.X} - Debug Log\n===========================================\n");
+            _writer.Flush();
+        }
+
+        public static void Close()
+        {
+            if (I != null)
+            {
+                Log("Closing log writer.");
+                I._writer.Close();
+            }
+            I = null;
+        }
+
+        private void _Log(string message)
+        {
+            _writer.WriteLine($"{DateTime.UtcNow:HH:mm:ss}: {message}");
+            _writer.Flush();
+        }
+        private void _LogException(Exception ex, Type callingType, string prefix = "")
+        {
+            if (ex == null)
+            {
+                _Log("Null exception! CallingType: " + callingType.FullName);
+                return;
+            }
+
+            _Log(prefix + $"Exception in {callingType.FullName}! {ex.Message}\n{ex.StackTrace}\n{ex.InnerException}");
+        }
+    }
+}
---
+++ Data/Scripts/AssemblyScripts/DebugUtils/SoftHandle.cs
@@ -0,0 +1,32 @@
+﻿using Sandbox.ModAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modular_Assemblies.Data.Scripts.AssemblyScripts.Debug
+{
+    public class SoftHandle
+    {
+        public static void RaiseException(string message, Exception ex = null, Type callingType = null, ulong callerId = ulong.MaxValue)
+        {
+            if (message == null)
+                return;
+
+            if (!MyAPIGateway.Utilities.IsDedicated)
+                MyAPIGateway.Utilities.ShowNotification("Minor Exception: " + message);
+            Exception soft = new Exception(message, ex);
+            ModularLog.LogException(soft, callingType ?? typeof(SoftHandle), callerId != ulong.MaxValue ? $"Shared exception from {callerId}: " : "");
+        }
+
+        public static void RaiseException(Exception exception, Type callingType = null, ulong callerId = ulong.MaxValue)
+        {
+            if (exception == null)
+                return;
+            if (!MyAPIGateway.Utilities.IsDedicated)
+                MyAPIGateway.Utilities.ShowNotification("Minor Exception: " + exception.Message);
+            ModularLog.LogException(exception, callingType ?? typeof(SoftHandle), callerId != ulong.MaxValue ? $"Shared exception from {callerId}: " : "");
+        }
+    }
+}
